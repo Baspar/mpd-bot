63ada6f611a5dd898d9065f92b35ce839f16a393
Bastien Laine <me@bastien-laine.io>
2020-04-18 15:18:27 +0800

    [add] local update ID counter

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use tokio::task::spawn_blocking;
 mod structs;
 use structs::{Update,Res};
 
-fn make_url(method: &str) -> String {
+fn make_url(method: String) -> String {
     let key = std::env::var("MPD_BOT_API_KEY").unwrap();
     format!("https://api.telegram.org/bot{}/{}", key, method)
 }
@@ -15,13 +15,20 @@ async fn process_update(update: &Update) {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut last_update_id: Option<i64> = None;
     loop {
-        let res: Res<Vec<Update>> = reqwest::get(&make_url("getUpdates"))
+        let method = match last_update_id {
+            None => String::from("getUpdates"),
+            Some(id) => format!("getUpdates?offset={}", id + 1)
+
+        };
+        let res: Res<Vec<Update>> = reqwest::get(&make_url(method))
             .await?
             .json()
             .await?;
         println!("{} updates", res.result.len());
         for update in &res.result {
+            last_update_id = Some(update.update_id);
             process_update(update).await;
         }
         delay_for(Duration::from_secs(1)).await;
